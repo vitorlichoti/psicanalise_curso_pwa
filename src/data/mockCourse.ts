30472293eac9455ef3312b4f0b52009144891212
vitor <vitorlichoti@gmail.com>
2023-09-22 14:08:49 -0400

    adiciona mock dos dados de um curso

---
+++ src/data/mockCourse.ts
@@ -0,0 +1,87 @@
+const courses = [
+    {
+        id: 1,
+        course_name: 'Especialização em Psicanálise Freudiana',
+        modules: [
+            {
+                id: 1,
+                module_name: 'Introdução',
+                classes: [
+                    {
+                        id: 1,
+                        title: 'PODCAST - Sigmund Freud',
+                        url: '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1598753283&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/psicanaliticamente" title="Academia Enlevo" target="_blank" style="color: #cccccc; text-decoration: none;">Academia Enlevo</a> · <a href="https://soundcloud.com/psicanaliticamente/episodio-47-sigmund-freud" title="Episódio 47 - Sigmund Freud" target="_blank" style="color: #cccccc; text-decoration: none;">Episódio 47 - Sigmund Freud</a></div>',
+                        content: {}
+                    },
+                    {
+                        id: 2,
+                        title: 'Breve história da psicanálise e sua relevância na sociedade',
+                        url: '',
+                        content: {
+                            title: 'Introdução à Psicanálise',
+                            paragraphs: [
+                                {
+                                    id: 1,
+                                    head: '',
+                                    text: 'A psicanálise, com sua complexidade teórica e profundidade na exploração da mente humana, não se limita apenas ao âmbito da psicologia; ela também desempenha um papel significativo na sociedade como um todo. Sigmund Freud, o pai da psicanálise, inaugurou uma revolução na compreensão da mente humana e dos processos inconscientes, gerando um impacto que transcende o campo da psicologia e alcança a sociedade em diversos aspectos.'
+                                },
+                                {
+                                    id: 2,
+                                    head: 'Desvendando o Inconsciente',
+                                    text: 'A psicanálise, com sua complexidade teórica e profundidade na exploração da mente humana, não se limita apenas ao âmbito da psicologia; ela também desempenha um papel significativo na sociedade como um todo. Sigmund Freud, o pai da psicanálise, inaugurou uma revolução na compreensão da mente humana e dos processos inconscientes, gerando um impacto que transcende o campo da psicologia e alcança a sociedade em diversos aspectos.'
+                                },
+                                {
+                                    id: 3,
+                                    head: 'Autoconhecimento e Relações Sociais',
+                                    text: 'A relevância da psicanálise na sociedade é mais evidente quando se trata do autoconhecimento e das interações humanas. Ao compreender os impulsos inconscientes que moldam nossas ações, podemos ganhar autoconsciência e tomar decisões mais informadas sobre nossas vidas. Além disso, a psicanálise oferece insights sobre os relacionamentos interpessoais, ajudando a decifrar dinâmicas complexas, como os padrões de comportamento repetitivos que surgem de experiências passadas.'
+                                },
+                                {
+                                    id: 4,
+                                    head: 'Conclusão',
+                                    text: 'A psicanálise, com sua investigação profunda do inconsciente humano e sua influência sobre diversos aspectos da vida, transcende o campo da psicologia e encontra uma relevância duradoura na sociedade. Ela não apenas fornece uma compreensão mais profunda de nós mesmos e dos outros, mas também molda a cultura, a criatividade, a saúde mental e as relações interpessoais. Através da psicanálise, o legado de Freud continua a enriquecer a compreensão da complexa tapeçaria da mente humana e suas implicações na sociedade em constante evolução.'
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        id: 3,
+                        title: 'PODCAST - Freud e a Antropologia',
+                        url: '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1598754900&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/psicanaliticamente" title="Academia Enlevo" target="_blank" style="color: #cccccc; text-decoration: none;">Academia Enlevo</a> · <a href="https://soundcloud.com/psicanaliticamente/episodio-48-freud-e-a-antropologia" title="Episódio 48 - Freud E A Antropologia" target="_blank" style="color: #cccccc; text-decoration: none;">Episódio 48 - Freud E A Antropologia</a></div>',
+                        content: {}
+                    },
+                ]
+            },
+            {
+                id: 2,
+                module_name: 'O Aparelho Psiquico',
+                classes: [
+                    {
+                        id: 1,
+                        title: 'Descrição detalhada da estrutura do aparelho psíquico segundo Freud: id, ego e superego',
+                        url: '',
+                        content: {
+                            title: 'Estrutura do aparelho psíquico segundo a teoria de Freud: id, ego e superego.',
+                            paragraphs: [
+                                {
+                                    id: 1,
+                                    head: '',
+                                    text: 'No vasto mundo da psicanálise, Sigmund Freud emergiu como um pioneiro, iluminando as profundezas da mente humana. Central para sua teoria está a concepção do aparelho psíquico, um modelo que delineia a complexa interação entre três entidades fundamentais: o id, o ego e o superego. Cada uma dessas instâncias opera com suas próprias forças, desejos e influências, moldando nossa experiência consciente e inconsciente. Neste artigo, exploraremos minuciosamente a estrutura do aparelho psíquico de acordo com a teoria de Freud, mergulhando nas características e nas interações do id, ego e superego.'
+                                },
+                                {
+                                    id: 2,
+                                    head: 'O Id: A Residência das Pulsões Primitivas',
+                                    text: 'O id é o alicerce mais primitivo do aparelho psíquico, localizado inteiramente no inconsciente. Regido pelo princípio do prazer, o id é movido por impulsos e desejos instintivos, buscando a gratificação imediata. É um reservatório de energias psíquicas, conhecidas como libido, que impulsionam os impulsos de sobrevivência e reprodução.'
+                                },
+                                {
+                                    id: 3,
+                                    head: 'Autoconhecimento e Relações Sociais',
+                                    text: 'A relevância da psicanálise na sociedade é mais evidente quando se trata do autoconhecimento e das interações humanas. Ao compreender os impulsos inconscientes que moldam nossas ações, podemos ganhar autoconsciência e tomar decisões mais informadas sobre nossas vidas. Além disso, a psicanálise oferece insights sobre os relacionamentos interpessoais. '
+                                }
+                            ]
+                        }
+                    }
+                ]
+            },
+        ],
+    },
+]
