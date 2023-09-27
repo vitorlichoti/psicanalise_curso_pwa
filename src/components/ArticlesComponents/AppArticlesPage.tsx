6108996a6ebffbd5351be1ecdd77097dd2720c2f
LuizHenriqueDelPosoVilalva <luizgarciaevilalva@gmail.com>
2023-09-27 10:32:36 -0400

    criado pagina inicial artigos

---
+++ src/components/ArticlesComponents/AppArticlesPage.tsx
@@ -0,0 +1,15 @@
+import TopBar from "@/components/Commons/TopBar";
+import BottomBar from "@/components/Commons/BottomBar";
+import Articles from "./UI/Articles";
+
+function AppArticlesPage() {
+    return (
+        <>
+            <TopBar />
+                <Articles />
+            <BottomBar />
+        </>
+    )
+}
+
+export default AppArticlesPage
